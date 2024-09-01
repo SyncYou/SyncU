f68ec404c37a63fffb95fc1cf95b3655a31a8cd6
Kalu David <kaludavidinyang@gmail.com>
2024-09-01 20:23:22 +0100

    changes on routing and file path

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,51 @@
 import React from "react";
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { SignUp } from "./pages/SignUp.jsx";
+import './components/styles/circleStyles.css'
+import './components/styles/skeleton.css'
+import './components/styles/reusable.css';
+import { Verify } from './components/Verify.jsx';
+import { Yourself } from './components/Yourself.jsx';
+import { Work } from './components/Work.jsx';
+import { Stack } from './components/Stack.jsx';
+import { Security } from './components/Security.jsx';
+import { Loader } from './components/Loader.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <SignUp />,
+  },
+  {
+    path: "/verify",
+    element: <Verify />
+  },
+  {
+    path: "/yourself",
+    element: <Yourself />
+  },
+  {
+    path: "/work",
+    element: <Work />
+  },
+  {
+    path: "/stack",
+    element: <Stack />
+  },
+  {
+    path: "/security",
+    element: <Security />
+  },
+  {
+    path: "/loader",
+    element: <Loader />
+  }
+
+]);
 
 function App() {
-  return <SignUp />
+  return <RouterProvider router={router} />
 }
 
 export default App
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,51 +1,21 @@
 import React from 'react';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-
-import { Welcome } from '../components/Welcome.jsx';
-import '../index.css';
-import '../components/styles/circleStyles.css'
-import '../components/styles/skeleton.css'
-import '../components/styles/reusable.css';
-import { Verify } from '../components/Verify.jsx';
-import { Yourself } from '../components/Yourself.jsx';
-import { Work } from '../components/Work.jsx';
-import { Stack } from '../components/Stack.jsx';
-import { Security } from '../components/Security.jsx';
-import { Loader } from '../components/Loader.jsx';
-
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Welcome />,
-  },
-  {
-    path: "/verify",
-    element: <Verify />
-  },
-  {
-    path: "/yourself",
-    element: <Yourself />
-  },
-  {
-    path: "/work",
-    element: <Work />
-  },
-  {
-    path: "/stack",
-    element: <Stack />
-  },
-  {
-    path: "/security",
-    element: <Security />
-  },
-  {
-    path: "/loader",
-    element: <Loader />
-  }
-
-]);
+import { Welcome } from '../components/Welcome';
+import logo from '../../public/comp-img/logo.svg';
 
 export function SignUp() {
-  return <RouterProvider router={router} />
+
+  return (
+    <>
+      <body className='signUpBody overflow-x-hidden w-[100%] h-[100%]'>
+        <section className="signUpSection section ">
+          <main className='signUpMain'>
+            <img src={logo} alt="syncU logo" />
+            <div>
+              <Welcome />
+            </div>
+          </main>
+        </section>
+      </body>
+    </>
+  )
 }
