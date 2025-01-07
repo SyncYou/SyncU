a55717fd5b88d7a68a0c7481d83469af618a846f
Black Bobs <ayodejiayodele350@gmail.com>
2025-01-07 04:35:11 +0100

    chores:did a little cleanup's

--- src/utils/SupabaseRequest.ts
+++ src/utils/SupabaseRequest.ts
@@ -35,46 +35,28 @@ export const sendUserDetails = async (userData: any) => {
   return {data, error};
 };
 
-// export const updateUserDetails = async (data: any) => {
-//   const { data: response, error } = await supabase
-//     .from("Users")
-//     .upsert(data)
-//     .select();
 
-//   if (error) {
-//     throw new Error(error.message);
-//   }
-//   console.log(response);
-//   return response;
-// };
 
 export async function uploadAvatar(file: File) {
-  // Get the file extension
   const fileExt = file.name.split(".").pop();
-  // Generate a random file name
   const fileName = `${Math.random()}.${fileExt}`;
-  // Define the file path for uploading
   const filePath = `avatar/${fileName}`;
 
   try {
-    // Upload the file to Supabase storage
     const { error: uploadError } = await supabase.storage
-      .from("avatar") // Ensure this bucket exists in your Supabase project
+      .from("avatar") 
       .upload(filePath, file);
 
-    // If there was an error during upload, throw it
     if (uploadError) {
       throw uploadError;
     }
 
-    // Retrieve the public URL of the uploaded file
     const {
       data: { publicUrl },
     } = supabase.storage.from("avatar").getPublicUrl(filePath);
 
-    console.log("Uploaded image URL:", publicUrl); // Log the URL for debugging
+    console.log("Uploaded image URL:", publicUrl); 
 
-    // Return the public URL of the uploaded image
     return publicUrl;
   } catch (error) {
     console.error("Error uploading image:", error);
