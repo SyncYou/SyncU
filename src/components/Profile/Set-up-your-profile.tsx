811b67878d68409c417a0b1442c080d3ba4e44e5
Victor Nwagbo <nwagbovictor98@.com>
2025-01-12 03:11:59 +0100

    signup revamping completed

--- src/components/Profile/Set-up-your-profile.tsx
+++ src/components/Profile/Set-up-your-profile.tsx
@@ -45,12 +45,12 @@ const SetUpYourProfile: React.FC = () => {
         <Header />
       </div>
       <div className="h-full w-full flex flex-col items-center justify-center mt-20 space-y-5">
-        <img className="my-5" src={profile} alt="" />
+        <img className="my-5 mb-1" src={profile} alt="" />
         <div>
-          <h2 className="font-semibold text-secondary text-center text-[28px] leading-[32px]">
+          <h2 className="font-semibold text-secondary text-center text-[2rem] leading-[32px]">
             Set up your profile🌟
           </h2>
-          <p className="text-gray text-center leading-6 font-normal text-[16px]">
+          <p className="text-gray text-center leading-6 font-normal text-[16px] my-5">
             Just a few more steps to go...
           </p>
         </div>
