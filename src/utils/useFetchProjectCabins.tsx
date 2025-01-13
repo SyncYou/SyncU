de6c3517f1542533d08d74679cec18ee186bfd3a
Kalu David <102557749+KaluDavid@users.noreply.github.com>
2025-01-13 15:47:51 +0100

    testing fetch error

--- src/utils/useFetchProjectCabins.tsx
+++ src/utils/useFetchProjectCabins.tsx
@@ -1,12 +1,11 @@
 import { supabase } from "../supabase/client";
-import { CardsData } from "./types/Types";
 
 export const useFetchProjectCabins = async () => {
   const { data, error } = await supabase.from("Projects").select("*");
   if (error) {
-    console.log(error);
     throw new Error("Cabins could not be loaded ");
   }
 
+  console.log("Fetched Projects:", data);
   return data;
 };
